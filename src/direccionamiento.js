cbff628af1221601b8abab8e818cbdc3d0bd97e9
Slevkro <mrkyloren117@gmail.com>
2020-11-10 19:56:36 -0600

    Separando por lineas la entrada e identificando la primera identacion

--- src/direccionamiento.js
+++ src/direccionamiento.js
@@ -1,7 +1,17 @@
 
 
 function check_sintaxis(data) {
-    console.log(data);
+    //console.log(data);
+    lines = data.split("\n");
+    for (var line in lines) {
+        //Only for testing, prints all
+        var identation = "    ";
+        console.log('LINEA '+(line)+' ----------');
+        console.log(lines[line]);
+        if (lines[line].substring(0, 4) == identation){ //Solo valida la primera identacion, puede haber mas?
+            console.log('...Identada banda...');
+        }
+    }
 }
 
 function tipo_direccionamiento(){
@@ -9,4 +19,4 @@ function tipo_direccionamiento(){
 
 }
 
-module.exports={ check_sintaxis, tipo_direccionamiento }
+module.exports={ check_sintaxis, tipo_direccionamiento}
