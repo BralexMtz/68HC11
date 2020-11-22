fd83c76ec47a1f7af79787ba0af93d6e7ec31eb0
Slevkro <mrkyloren117@gmail.com>
2020-11-21 22:57:06 -0600

    Cambiando de paradigma

--- src/compilation.js
+++ src/compilation.js
@@ -3,131 +3,202 @@ const xlsxFile = require('read-excel-file/node');
 var lines=[]; 
 var values=[];
 var etiquetas=[];
+const directives=["ORG","END","EQU","FCB"]
 
-function check_syntax(data) {
-    //console.log(data);
-    lines = data.split("\n");
+function linea(){
+    this.tipo; //  ETIQUETA, INSTRUCCION, VARIABLE, COMENTARIO
+    this.instruccion;
+    this.operando=[]
+    this.errores=[]
+    this.is_identado;
+    this.comentario;
+    
+    this.get_bytes= function (numOperando) {
+        var operatorBytes;
+        operatorBytes = operador[numOperando].replace('#', '')
+        operatorBytes = operatorBytes.replace('$', '')
+        return operatorBytes.length/2 
+    }
+}
+
+
+
+function is_identado(linea_str) {
     var rexp = /^[\s]\w*.*/;
-    for (var line in lines){
-        //Only for testing, prints all
-        console.log(lines[line].match(rexp));        
+
+    if(linea_str.match(rexp)==null){
+        return false;
+    }else{
+        return true;
     }
 }
 
 function tipo_direccionamiento(){
     //Funcion de prueba
-    console.log('Detectando el tipo...');
+    console.log(lines);
     for (line in lines){
         //REVISAR SI YA LLEGAMOS A LA LINEA CON EL END
-        if (line[lines][0] == 'END'){
+        lineLength = lines[line].length;
+        if (lines[line][0] == 'END'){
             console.log('SALAVERGA YA TERMINO...')
             break
         }
-            
-
-        lineLength = line[lines].length()
-        if (lineLength == 1){
+        if (directives.includes(lines[line][0])){
+            console.log('Es directiva mi chavo :u' + lines[line][0])
+        }else if (lineLength == 1){
             //POSIBLE MODO INHERENTE, REVISAR QUE EL MNEMONICO TENGA MODO INHERENTE
-            
+            console.log('INERENTE SIN OPERANDO :c' + lines[line][0])
         }else if(lineLength > 1 && lineLength != 0){
-            //Alguno de los ortros
-            operator = line[lines][1]
+            //Alguno de los otros
+            operator = lines[line][1]
             operatorBytes = operator.replace('#', '')
             operatorBytes = operatorBytes.replace('$', '')
-            numOperatorBytes = operatorBytes.length()/2
+            numOperatorBytes = operatorBytes.length/2
             if (!operator.startsWith('$')){
                 //PASARLO A HEXADECIMAL
             }
             if (operator.startsWith('#')){
                 //POSIBLE MODO INMEDIATO, REVISAR EL EL EXCEL QUE COINCIDA
-                console.log('INMEDIATO CON #' + operator)    
+                console.log('INMEDIATO CON # ' + lines[line][0])    
             }else if (operator.endsWith(',X')){
                 //POSIBLE INDEXADO CON RESPECTO DE X
-                console.log('INDEXADO CON RESPECTO DE X' + operator)
+                console.log('INDEXADO CON RESPECTO DE X '  + lines[line][0])
             }else if (operator.endsWith(',Y')){
                 //POSIBLE INDEXADO CON RESPECTO DE Y
-                console.log('INDEXADO CON RESPECTO DE Y' + operator)
+                console.log('INDEXADO CON RESPECTO DE Y ' + lines[line][0])
             }else if (numOperatorBytes == 2){
-                console.log('EXTENDIDO DE 2 BYTES' + operator)
+                console.log('EXTENDIDO DE 2 BYTES ' + lines[line][0])
             }else if (numOperatorBytes == 1){
-                console.log('DIRECTO DE 1 BYTE' + operator)
+                console.log('DIRECTO DE 1 BYTE ' + lines[line][0])
             }
         }
         //SI NO CAE EN NINGUNO DE LOS CASOS LA  LINEA ESTA VACIA
     }
 }
 
-function lectura_excel(rows){
-    console.log("rows");
+
+function is_excepcion(instruccion){
+    let excepcion=["BSET","BRCLR","BCLR","BRSET"]
+    return excepcion.includes(instruccion)    
+    
 }
 
-function varConst(lines){
-    var variables=[]
-    for (var line in lines){
-        if (lines[line][1] == 'EQU')
-            variables[lines[line][0]] =  lines[line][2]
+function get_operandos_exception (instruccion, operandos){
+    var auxiliar;
+    var resultado=[]
+    if (operandos.includes(',X')){
+        auxiliar = operandos.replace(',X', '...')
+        resultado = auxiliar.split(',')
+        resultado[0] = resultado[0].replace('...', ',X')
+        
+    }else if (operandos.includes(',Y')){
+        auxiliar = operandos.replace(',Y', '...')
+        resultado = auxiliar.split(',')
+        resultado[0] = resultado[0].replace('...', ',Y')
+        
+    }else{
+        resultado = operandos.split(',')
     }
-    return variables
-}
 
+    return resultado;
+    
+}
 
 function get_lines(data){
     lineas = data.split("\n"); //separa por renglon
-    for (var line in lineas) {
-        if(lineas[line].indexOf("*")!= -1)
-            lineas[line]=lineas[line].substring(0,lineas[line].indexOf("*"));//quitar comentarios de cada linea
+    var renglones=[]
+    for (var index in lineas) {
+        let comentario;
+        renglones[index]=lineas[index];
+        if(lineas[index].indexOf("*") != -1){
+            //renglon sin comentario  
+            renglones[index]=lineas[index].substring(0,lineas[index].indexOf("*"));//quitar comentarios de cada linea
+            //comentario
+            comentario=lineas[index].substring(lineas[index].indexOf("*"));
+        }
         //Read the lineas and separate with comas
-        lineas[line] = lineas[line].replace(/\s+/g, ' ').trim();
-        //Create an multidimensional array, [0] is INSTRUCTION(MNEMONICO) [1] OPERANDOS [2-*] COMMENT
-        lineas[line] = lineas[line].split(' ');
+        renglones[index] = renglones[index].replace(/\s+/g,' ').trim();
+        //Create an multidimensional array, [0] is INSTRUCTION(MNEMONICO) [1] OPERANDOS/EQU [2]? VALOR DE EQU
+        renglones[index] = renglones[index].split(' ');
+        longitud = renglones[index].length;
+        linea_X =  new linea();
+        if(longitud == 1 && renglones[index][0].length != 0){
+            //ES ETIQUETA
+            linea_X.tipo = 'ETIQUETA';
+            etiquetas[renglones[index][0]]="x"
+        }else if((longitud == 2 || longitud == 3) && is_identado(lineas[index])){
+            //ES UN MNEMONICO CON OPERANDOS
+            linea_X.tipo = 'INSTRUCCION'
+            linea_X.instruccion=renglones[index][0]
+            linea_X.comentario = comentario
+            if(is_excepcion(linea_X.instruccion)){
+                linea_X.operando=get_operandos_exception(linea_X.instruccion, renglones[index][1])
+                if(longitud==3){
+                    linea_X.operando.push(renglones[index][2])
+                }
+                console.log(linea_X)    
+            }else{
+                linea_X.operando.push(renglones[index][1])
+            }
+            linea_X.is_identado=is_identado(lineas[index])
+
+        }else if (renglones[index].includes("EQU")){
+            //ES UNA VARIABLE O CONSTANTE
+            linea_X.tipo = 'VARIABLE'
+            linea_X.comentario = comentario
+            values[renglones[index][0]] =  renglones[index][2]
+        }else{
+            //ES PURO COMENTARIO O NADA
+            linea_X.tipo = 'COMENTARIO'
+            linea_X.comentario = comentario
+        }
+        renglones[index]=linea_X
     }
-    return lineas;
+    return renglones;
     
 }
 
 function exist_mnemonicos(rows){
     var bandera = 0;
    
-    for (var inst in lines){
-        for (var num = 2 ; num<=99 ; num++){
-            if(lines[inst][0].toUpperCase() == rows[num][0].toUpperCase()){
-                //console.log("SIMON KRNAL QUE LISTO");
-                bandera = 1;
-            }    
+    for (var linea_obj of lines){
+        if(linea_obj.tipo=="INSTRUCCION"){
+            for (var num = 2 ; num<=99 ; num++){
+                if(linea_obj.instruccion.toUpperCase() == rows[num][0].toUpperCase()){
+                    bandera = 1; // SI HAY NMEMONICO en excel
+                }                
+            }
+            if(bandera == 0){
+                linea_obj.errores.push(4) //agregar a los errores
+            }
+            bandera=0;
         }
-        if(bandera == 0){
-             if( !Object.keys(values).includes(lines[inst][0].toUpperCase()) && lines[inst][0]!=''){//NO se trata de una variable    
-                lines[inst].push('4')// 
-             }
-             
-        }
-                //console.log("NEL TAS WEY ESTO NO EXISTE");
-        bandera=0;
     }
 }
 
-function get_etiquetas(lines){
-    //no tiene operados ?
-    //debe estar sin identacion
-    // despues del org 
-    
-}
+
 
 
 function main(data){
     //assets/INSTRUCCIONES.xlsx ====> FINAL LINE, BUT NOW FOR TESTING 
     xlsxFile('../assets/INSTRUCCIONES.xlsx').then((rows)=>{
         lines = get_lines(data)
-        values = varConst(lines); //obtener constantes
-        etiquetas = get_etiquetas(lines);
-
-        exist_mnemonicos(rows);        // CHECK IF EXIST THE INSTRUCCION
-        console.log(lines)
         
+        exist_mnemonicos(rows);        // CHECK IF EXIST THE INSTRUCCION
+        
+
+
+        tipo_direccionamiento()
+        
+
+
+        //escribir archivos LST
         var fs = require('fs');
         //src/nmms.txt >>final text
         fs.appendFile('nmms.txt','line', function (err) {
         if (err) throw err;
+
+
             
         });        
     });
--- src/direccionamiento.js
+++ src/direccionamiento.js
@@ -1,56 +1,11 @@
-function check_sintaxis(data) {
-    //console.log(data);
-    lines = data.split("\n");
-    for (var line in lines) {
-        //Only for testing, prints all
-        var identation = "    ";
-        console.log('LINEA '+(line)+' ----------');
-        console.log(lines[line]);
-        if (lines[line].substring(0, 4) == identation){ //Solo valida la primera identacion, puede haber mas?
-            console.log('...Identada banda...');
-        }
+
+
+function is_identado(linea_str) {
+    var rexp = /^[\s]\w*.*/;
+        
+    if(linea_str.match(rexp)==null){
+        return false;
+    }else{
+        return true;
     }
-
-    //REVISAR QUE EN LAS LINEAS HAYA POR LO MENOS UN END
 }
-
-function tipo_direccionamiento(lines){
-    //Funcion de prueba
-    console.log('Detectando el tipo...');
-    
-
-}
-
-function lectura_excel(rows){
-    console.log("rows");
-}
-
-function revisar_existencia(data,rows){
-    lines = data.split("\n");
-    for (var line in lines) {
-        //Read the lines and separate with comas
-        lines[line] = lines[line].replace(/\s+/g, ' ').trim().replace(/\s/g,",");
-        //Create an multidimensional array, [0] is INSTRUCTION(MNEMONICO) [1] OPERANDOS [2-*] COMMENT
-        lines[line] = lines[line].split(',');
-    }
-    // CHECK IF EXIST THE INSTRUCCION
-    var bandera = 0;
-    for (var inst in lines){
-        for (var num = 2 ; num<=99 ; num++){
-            if(lines[inst][0].toUpperCase() == rows[num][0].toUpperCase()){
-                console.log("SIMON KRNAL QUE LISTO");
-                bandera = 1;
-            }    
-        }
-        if(bandera == 0)
-                console.log("NEL TAS WEY ESTO NO EXISTE")
-        bandera=0;
-    }
-    console.log(rows)
-    console.log(lines)
-}
-
-module.exports={ check_sintaxis,
- tipo_direccionamiento,
-lectura_excel,
-revisar_existencia}
